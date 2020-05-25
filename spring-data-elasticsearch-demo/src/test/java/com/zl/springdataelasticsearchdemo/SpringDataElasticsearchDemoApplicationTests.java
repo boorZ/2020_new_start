2c91e15427bbe34126996b24b4411284da68de31
周林 <zhoulin5>
2020-05-25 22:29:17 +0800

    初始化SpringDataElasticsearchDemo项目

---
+++ spring-data-elasticsearch-demo/src/test/java/com/zl/springdataelasticsearchdemo/SpringDataElasticsearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zl.springdataelasticsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
