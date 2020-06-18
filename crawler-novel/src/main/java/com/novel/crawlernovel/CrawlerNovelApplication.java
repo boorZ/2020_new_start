92ead81a7d9055674ae74e0997b82024204cf36f
zhoulin <prometheus@noask-ai.com>
2020-06-18 21:37:13 +0800

    初始化爬虫——小说项目

---
+++ crawler-novel/src/main/java/com/novel/crawlernovel/CrawlerNovelApplication.java
@@ -0,0 +1,13 @@
+package com.novel.crawlernovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerNovelApplication.class, args);
+    }
+
+}
---
+++ crawler-novel/src/test/java/com/novel/crawlernovel/CrawlerNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novel.crawlernovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
